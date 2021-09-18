462b61945b2dfcb3d22c518af361ce9b20a74731
FatzryahVirantika <88219019+FatzryahVirantika@users.noreply.github.com>
2021-09-18 09:54:56 +0700

    Update koneksi.php

--- function/koneksi.php
+++ function/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "weshop";
+	$server = "aa9ws3jo1fgegy.ceyffbcurh55.us-east-1.rds.amazonaws.com";
+	$username = "admin";
+	$password = "admin123";
+	$database = "ebdb";
 	
 	$koneksi = mysqli_connect($server, $username, $password, $database) or die("Koneksi ke database gagal");
 	
